dc35740cc98e99a187ff79223ef9bc7d3b665d74
haqqii <61973814+haqqii@users.noreply.github.com>
2023-08-21 12:16:52 +0700

    1 file modified

--- src/view/login.vue
+++ src/view/login.vue
@@ -1,47 +1,41 @@
 <template>
-  <div class="container">
-    <form @submit.prevent="login">
-      <h2 class="mb-3">Login</h2>
-      <div class="input">
-        <label for="email">Email</label>
-        <input
-          class="form-control"
-          type="text"
-          name="email"
-          placeholder="email@adress.com"
-        />
+  <div class="login-container">
+    <div class="logo-container">
+      <img src="path-to-your-logo.png" alt="Logo" class="logo" />
+    </div>
+    <div class="form-container">
+      <div class="form-box">
+        <h2 class="form-title">Login</h2>
+        <form @submit.prevent="submitForm" class="form">
+          <div class="form-group">
+            <label for="email" class="form-label">Email</label>
+            <input
+              type="email"
+              id="email"
+              v-model="email"
+              class="form-input"
+              required
+            />
+          </div>
+          <div class="form-group">
+            <label for="password" class="form-label">Password</label>
+            <input
+              type="password"
+              id="password"
+              v-model="password"
+              class="form-input"
+              required
+            />
+          </div>
+          <button type="submit" class="btn btn-primary form-button">
+            Login
+          </button>
+        </form>
       </div>
-      <div class="input">
-        <label for="password">Password</label>
-        <input
-          class="form-control"
-          type="password"
-          name="password"
-          placeholder="password123"
-        />
-      </div>
-      <div class="alternative-option mt-4">
-        You don't have an account? <span @click="moveToRegister">Register</span>
-      </div>
-      <button type="submit" class="mt-4 btn-pers" id="login_button">
-        Login
-      </button>
-      <div
-        class="alert alert-warning alert-dismissible fade show mt-5 d-none"
-        role="alert"
-        id="alert_1"
-      >
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-        <button
-          type="button"
-          class="btn-close"
-          data-bs-dismiss="alert"
-          aria-label="Close"
-        ></button>
-      </div>
-    </form>
+    </div>
   </div>
 </template>
+
 <script>
 export default {
   data() {
@@ -51,20 +45,84 @@ export default {
     };
   },
   methods: {
-    login() {
-      // Implement your login logic here
-      console.log('Login attempt:', this.email, this.password);
+    submitForm() {
+      // Lakukan aksi login atau validasi di sini
+      // Misalnya, Anda bisa mengirimkan data ke server atau melakukan validasi di sisi klien
+      console.log('Email:', this.email);
+      console.log('Password:', this.password);
     },
   },
 };
 </script>
 
 <style scoped>
-/* Center the login form */
-.container {
+.login-container {
   display: flex;
   justify-content: center;
   align-items: center;
-  min-height: 100vh;
+  height: 100vh;
+}
+
+.logo-container {
+  text-align: center;
+  margin-bottom: 20px;
+}
+
+.logo {
+  align-items: center;
+  max-width: 100px;
+}
+
+.form-container {
+  width: 300px;
+}
+
+.form-box {
+  background-color: #fff;
+  border: 1px solid #e1e1e1;
+  padding: 20px;
+  border-radius: 8px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+}
+
+.form-title {
+  font-size: 24px;
+  margin-bottom: 20px;
+}
+
+.form {
+  display: flex;
+  flex-direction: column;
+}
+
+.form-group {
+  margin-bottom: 15px;
+}
+
+.form-label {
+  font-weight: bold;
+  font-size: 14px;
+  margin-bottom: 5px;
+}
+
+.form-input {
+  padding: 10px;
+  border: 1px solid #e1e1e1;
+  border-radius: 4px;
+  display: flex;
+}
+
+.form-button {
+  padding: 10px;
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: background-color 0.2s;
+}
+
+.form-button:hover {
+  background-color: #0056b3;
 }
 </style>
